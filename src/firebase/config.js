0e8745322d4001124c15d6b68c427e80f27fc9a7
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2021-02-05 01:08:08 +0100

    Create a new app with React :-)

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDpNTHf4COFPdrfPoiF7tb22180lyQ27c4",
+    authDomain: "firegram-64b8a.firebaseapp.com",
+    projectId: "firegram-64b8a",
+    storageBucket: "firegram-64b8a.appspot.com",
+    messagingSenderId: "655294158205",
+    appId: "1:655294158205:web:b1d6536407a5b21f5de814"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
