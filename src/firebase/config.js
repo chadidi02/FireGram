49ce600c2f39216ccf7d467ca9e163028c58c3c2
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2021-02-07 01:46:09 +0100

    (•_•)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,3 +1,8 @@
+// import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+import firebase from 'firebase/app'
+
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyDpNTHf4COFPdrfPoiF7tb22180lyQ27c4",
@@ -7,5 +12,13 @@ var firebaseConfig = {
     messagingSenderId: "655294158205",
     appId: "1:655294158205:web:b1d6536407a5b21f5de814"
 };
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
